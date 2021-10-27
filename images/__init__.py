993fc672d0327014a48fdfb5bf812cfe53cd3cc3
bogumilchilinski <bogumil.chilinski@gmail.com>
2021-10-27 11:07:45 +0000

    Daily upload - dgeometry

---
+++ images/__init__.py
@@ -0,0 +1,10 @@
+"""
+This module provides basic tools for fundamental operations on geometrical objects
+"""
+
+
+
+# from .dynamics import (LagrangesDynamicSystem, LinearDynamicSystem,
+#                        HarmonicOscillator, WeakNonlinearOscillator, DampedHarmonicOscillator)
+
+from ..dgeometry import *
